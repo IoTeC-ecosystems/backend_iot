91b2adedbda7c4721daf2071cce75437d5ddd791
Jose Septien <jaseptienh@gmail.com>
2025-07-14 17:33:22 -0600

    Add function to create app with flask.

---
+++ backend_iot/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        # Load the instanc config, if it exists, when not testing
+        app.config.from_pyfile('instance_config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, IoT World!\n'
+
+    return app
